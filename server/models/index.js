71a6322c419df11b90ef1543f6577d43a6e212d4
Sigurd Gartmann <sigurdga@sigurdga.no>
2015-06-20 19:10:09 +0200

    End migration

--- server/models/index.js
+++ server/models/index.js
@@ -24,17 +24,6 @@ var OrganizationSchema = new mongoose.Schema({
 });
 
 var ProjectSchema = new mongoose.Schema({
-    name: {type: String, trim: true, required: true},
-    client: {type: String, trim: true, ref: 'Client'},
-    user: {type: String, ref: 'User', required: true}, // user who created project
-    organization: {type: String, ref: 'Organization', required: true},
-    users: [{type: String, ref: 'User'}], // deprecated
-    active: {type: Date},
-    created: {type: Date, required: true, 'default': Date.now},
-    modified: {type: Date, required: true, 'default': Date.now}
-});
-
-var NewProjectSchema = new mongoose.Schema({
     _id: {type: String, required: true, unique: true, 'default': shortid.generate},
     name: {type: String, trim: true, required: true},
     client: {type: String, trim: true, ref: 'Client'},
@@ -44,23 +33,10 @@ var NewProjectSchema = new mongoose.Schema({
     active: {type: Date},
     created: {type: Date, required: true, 'default': Date.now},
     modified: {type: Date, required: true, 'default': Date.now}
-});
-
-var NewHoursSchema = new mongoose.Schema({
-    _id: {type: String, required: true, unique: true, 'default': shortid.generate},
-    user: {type: String, ref: 'User', required: true},
-    project: {type: String, ref: 'Project', required: true},
-    date: {type: Date, required: true},
-    start: {type: Date}, // use this for date
-    end: {type: Date},
-    duration: {type: Number, required: true},
-    comment: {type: String, trim: true},
-    created: {type: Date, required: true, 'default': Date.now},
-    invoice: {type: String, ref: 'Invoice'}
 });
 
 var HoursSchema = new mongoose.Schema({
-    //_id: {type: String, required: true, unique: true, 'default': shortid.generate},
+    _id: {type: String, required: true, unique: true, 'default': shortid.generate},
     user: {type: String, ref: 'User', required: true},
     project: {type: String, ref: 'Project', required: true},
     date: {type: Date, required: true},

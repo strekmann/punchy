77ac52c52cd835324e91fff1327b514835ed0bad
Jørgen Bergquist <jorgen.bergquist@ntnu.no>
2017-02-16 17:44:25 +0100

    common

---
+++ src/common/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Relay from 'react-relay';
+import {
+    Route,
+    IndexRoute,
+    createRoutes,
+} from 'react-router';
+
+import App from './components/App';
+import NotFound from './components/NotFound';
+
+export const viewerQuery = {
+    viewer: () => Relay.QL`query { viewer }`,
+};
+
+export default createRoutes(
+    <Route path="/" component={App} queries={viewerQuery}>
+        <IndexRoute component={Index} queries={viewerQuery} />
+        <Route path="*" component={NotFound} />
+    </Route>
+);

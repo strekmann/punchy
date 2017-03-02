2677519ae92cd358174b2345e6465cba963e6393
Jørgen Bergquist <jorgen.bergquist@ntnu.no>
2017-03-02 19:12:01 +0100

    landing knot

--- src/common/components/Landing.js
+++ src/common/components/Landing.js
@@ -7,79 +7,54 @@ class Landing extends React.Component {
     }
     render() {
         const { site } = this.props;
-
-        const style = {
-            header: {
-                fontWeight: 300,
-                color: '#202020',
-                fontSize: '15px',
-                lineHeight: '21px',
-                height: '50px',
-                background: 'white',
-                boxShadow: 'inset 0 -1px 0 0 rgba(0,0,0,.1)',
-                zIndex: 10,
-            },
-            section: {
-                maxWidth: 1080,
-                margin: '0 auto',
-                padding: '0 20px',
-            },
-            home: {
-                float: 'left',
-                fontSize: '18px',
-                textDecoration: 'none',
-                paddingRight: '1em',
-                lineHeight: '50px',
-                textTransform: 'uppercase',
-                color: '#202020',
-            },
-            hero: {
-                position: 'relative',
-                background: '#171E26',
-            },
-            heroheader: {
-                background: 'linear-gradient(rgba(23, 30, 38, 0.8), rgba(23, 30, 38, 0))',
-                boxShadow: 'none',
-                display: 'block',
-            },
-            intro: {
-                margin: '0 auto',
-                padding: '0 20px',
-                clear: 'both',
-                display: 'flex',
-                maxWidth: '1070px',
-                flexDirection: 'column',
-            },
-        };
-
         return (
             <div id="landingpage">
-                <header style={style.header}>
-                    <section style={style.section}>
-                        <a href="#landingpage" style={style.home}>{site.name}</a>
+                <header>
+                    <section>
+                        <a className="nav-home" href="#landingpage">{site.name}</a>
                         <nav>
-                            <a href="#test">Test</a>
+                            <a href="#test">Top header</a>
                         </nav>
                     </section>
                 </header>
 
                 <div className="hero">
                     <div className="abs">
-                        <header aria-hidden="true" style={style.heroheader}>
-                            <nav>
-                                <a href="#test">Test</a>
-                            </nav>
+                        <header aria-hidden="true">
+                            <section>
+                                <nav>
+                                    <a href="#test">Test</a>
+                                </nav>
+                            </section>
                         </header>
-                        <section className="intro" style={style.into}>
-                            Intro something here
+                        <section className="intro">
+                            <div>
+                                <h1 style={{ fontSize: '58px' }}>Punchy</h1>
+                            </div>
+                            <div>
+                                <h2 style={{ fontSize: '16px', textTransform: 'uppercase' }}>Timeregistrering for vanlige mennesker</h2>
+                            </div>
                         </section>
                     </div>
                 </div>
 
-                <section className="lead" style={style.section}>
+                <section className="lead">
                     <h1>Derp derp</h1>
                     <p>Some crap</p>
                 </section>
+
+                <div className="grayWash">
+                    <section className="point1">
+                        <h1>Herp herp</h1>
+                        <p>Some moar crap</p>
+                    </section>
+                </div>
+
+                <footer>
+                    <section>
+                        some footer stuff
+                    </section>
+                </footer>
             </div>
         );
     }

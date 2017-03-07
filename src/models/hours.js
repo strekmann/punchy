d8098bf27fffbda33f1e18a680e12084eda188cd
Jørgen Bergquist <jorgen.bergquist@ntnu.no>
2017-03-07 17:53:53 +0100

    Fix models

--- src/models/hours.js
+++ src/models/hours.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import uuid from 'uuid/v4';
 
 const HoursSchema = new mongoose.Schema({
-    _id: { type: String, required: true, unique: true, default: uuid },
+    _id: { type: String, required: true, default: uuid },
     user: { type: String, ref: 'User', required: true },
     project: { type: String, ref: 'Project', required: true },
     date: { type: Date, required: true },
@@ -10,14 +10,16 @@ const HoursSchema = new mongoose.Schema({
     end: { type: Date },
     duration: { type: Number, required: true },
     comment: { type: String, trim: true },
-    created: { type: Date, required: true, default: Date.now },
     invoice: { type: String, ref: 'Invoice' },
+}, {
+    timestamps: {
+        createdAt: 'created',
+    },
 });
 
 const opts = { versionKey: false, virtuals: true };
 HoursSchema.set('toObject', opts);
 HoursSchema.set('toJSON', opts);
-HoursSchema.set('timestamps', true);
 HoursSchema.virtual('$type').get(() => {
     return 'Hours';
 });
--- src/models/organization.js
+++ src/models/organization.js
@@ -2,17 +2,19 @@ import mongoose from 'mongoose';
 import uuid from 'uuid/v4';
 
 const OrganizationSchema = new mongoose.Schema({
-    _id: { type: String, required: true, unique: true, default: uuid },
+    _id: { type: String, required: true, default: uuid },
     admins: [{ type: String, ref: 'User' }],
     name: { type: String, trim: true, required: true },
     users: [{ type: String, ref: 'User' }],
-    created: { type: Date, required: true, default: Date.now },
+}, {
+    timestamps: {
+        createdAt: 'created',
+    },
 });
 
 const opts = { versionKey: false, virtuals: true };
 OrganizationSchema.set('toObject', opts);
 OrganizationSchema.set('toJSON', opts);
-OrganizationSchema.set('timestamps', true);
 OrganizationSchema.virtual('$type').get(() => {
     return 'Organization';
 });
